0e75593dbebcdc8c95893e1fcdb9b8baa2dd0da5
Petar <petar.milisavljevic@elfak.rs>
2023-07-23 14:12:43 +0200

    updated filter requests

--- apps/api/dist/modules/group/dto/requests/fitler-group.request.d.ts
+++ apps/api/dist/modules/group/dto/requests/fitler-group.request.d.ts
@@ -1,7 +1,10 @@
 import { Group } from '@/core/entities';
 import { QueryEntity } from '@/core/types';
-export type GroupRelationTypes = 'attributes';
+declare const POPULATE: string[];
+export type GroupRelationTuple = typeof POPULATE;
+export type GroupRelationTypes = GroupRelationTuple[number];
 export declare class QueryGroup extends QueryEntity<GroupRelationTypes, Group> {
     include?: GroupRelationTypes[];
 }
+export {};
 //# sourceMappingURL=fitler-group.request.d.ts.map
--- apps/api/src/modules/group/dto/requests/fitler-group.request.ts
+++ apps/api/src/modules/group/dto/requests/fitler-group.request.ts
@@ -1,7 +1,15 @@
 import { Group } from '@/core/entities';
 import { QueryEntity } from '@/core/types';
 
-export type GroupRelationTypes = 'attributes';
+const POPULATE = [
+  'attributes',
+  'attributes.group',
+  'attributes.options.configs',
+];
+
+export type GroupRelationTuple = typeof POPULATE;
+
+export type GroupRelationTypes = GroupRelationTuple[number];
 
 export class QueryGroup extends QueryEntity<GroupRelationTypes, Group> {
   include?: GroupRelationTypes[];
